34c94c79a5a5872632f13a935e387064c0e62cca
kingspp <kingspprathyush@gmail.com>
2014-08-17 22:25:31 +0530

    Added Problem1 in java

--- Java/Problem1.java
+++ Java/Problem1.java
@@ -1,7 +1,22 @@
 public class Problem1 {
 
 	public static void main(String v[]) {
-		System.out.println("Hello World");
-	}
+		double start = System.nanoTime();
+		int num = 0;
 
+		System.out.println("Find Multiples of 3 and 5");
+
+		for (int i = 3; i < 1000; i++) {
+			if (i % 3 == 0) {
+				num += i;
+			}
+			if (i % 5 == 0) {
+				num += i;
+			}
+		}
+		System.out.println(num);
+		double end = System.nanoTime();
+		System.out.println("Time Elapsed is: " + String.valueOf((end - start))
+				+ "ns");
+	}
 }
