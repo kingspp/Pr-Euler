d97a00a608196e6fab291e8228d08545eca47b55
kingspp <kingspprathyush@gmail.com>
2014-08-17 22:47:21 +0530

    Added Problem1 in C

--- C/Problem1.c
+++ C/Problem1.c
@@ -7,11 +7,27 @@
 
 
 #include <stdio.h>
+#include <time.h>
 
 int main()
 {
-	printf("Hello");
+	int i=0,num=0;
+	time_t start,end;
+	time (&start);
+	printf("Sum of Multiples of 3 and 5\n");
+	for (i = 3; i < 1000; i++) {
+				if (i % 3 == 0) {
+					num += i;
+				}
+				if (i % 5 == 0) {
+					num += i;
+				}
+			}
 
+	printf ("%d \n",num);
+	time (&end);
+	double dif = difftime (end,start);
+	printf ("Elasped time is %.2lf seconds.", dif );
 
 	return 0;
 }
