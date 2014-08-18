8b64114e2ce29d0d0af49ed2b89af71022602231
kingspp <kingspprathyush@gmail.com>
2014-08-18 21:50:28 +0530

    Modified Problem6

--- Python/Problem6.py
+++ Python/Problem6.py
@@ -5,12 +5,12 @@ Created on Aug 16, 2014
 '''
 
 import math
-from main import test
+import time
 
 print "Difference between the sum of the squares of the first one hundred natural numbers and the square of the sum"
 SUM1=0
 SUM2=0
-start=test.Time()
+start=time.time()
 
 for i in xrange(1,100+1):
     SUM1+=i
@@ -23,7 +23,7 @@ SUM1=SUM1**2
 print SUM1-SUM2
 
 
-end=test.Time()
+end=time.time()
 
 print 'The test elapsed is: ' +str(end-start)+ 's'
 
